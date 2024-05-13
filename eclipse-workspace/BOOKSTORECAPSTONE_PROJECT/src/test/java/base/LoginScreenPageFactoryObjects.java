f50e9d8485769e078ec23b89c787e88c093bbd2c
Administrator <Administrator@172.19.8.109>
2024-05-13 21:09:50 +0530

    AddedAgain

--- eclipse-workspace/BOOKSTORECAPSTONE_PROJECT/src/test/java/base/LoginScreenPageFactoryObjects.java
+++ eclipse-workspace/BOOKSTORECAPSTONE_PROJECT/src/test/java/base/LoginScreenPageFactoryObjects.java
@@ -4,6 +4,6 @@ public class LoginScreenPageFactoryObjects {
 
 	public static final String menuSearch_xpath = "(//*[@id=\"menuSearch\"])[1]";
 	public static final String searchBar_xpath	="//*[@id='autoComplete']";
-	
+	//
 	
 }
--- eclipse-workspace/BOOKSTORECAPSTONE_PROJECT/src/test/java/systemTests/WelcomeScreen.java
+++ eclipse-workspace/BOOKSTORECAPSTONE_PROJECT/src/test/java/systemTests/WelcomeScreen.java
@@ -3,5 +3,7 @@ package systemTests;
 import base.BaseClass;
 
 public class WelcomeScreen extends BaseClass{
+	
+	
 
 }
