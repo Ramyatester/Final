d299ea33d010ec60446c2e4955c898d40a74f535
Administrator <Administrator@172.19.8.109>
2024-05-14 20:18:57 +0530

    dd

--- eclipse-workspace/BOOKSTORE_CAPSTONEPROJECT/src/test/java/systemTests/LoginScreen.java
+++ eclipse-workspace/BOOKSTORE_CAPSTONEPROJECT/src/test/java/systemTests/LoginScreen.java
@@ -1,48 +1,98 @@
 package systemTests;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import base.BaseClass;
+import base.LocType;
+import base.LoginScreenPageFactoryObjects;
+import base.WelcomeScreenPageFactoryObjects;
 
 /* Authour of Test :Ramya-194189*/
 
 public class LoginScreen extends BaseClass {
 
-	// Login unsuccesful with user name : Testerauto1 , Password : Google@123
-	@Test(priority = 1)
-	public void verifyInvalidLogin_InvalidUserName() {
-
-		driver.findElement(By.xpath("//a[normalize-space()='Login or register']")).click();
-		driver.findElement(By.xpath("//input[@id='loginFrm_loginname']")).sendKeys("Testerauto1");
-
-		driver.findElement(By.xpath("//input[@id='loginFrm_password']")).sendKeys("Google@123");
-
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+	// Login unsuccessful with user name : Testerauto1 , Password : Google@123
+	@Test(priority = 1) 
+	public void verifyInvalidLogin_InvalidUserName() throws Throwable{
+		
+		
+		try {
+			
+			buttonClick(LoginScreenPageFactoryObjects.LoginOrRegisterButton);
+			enterText(LoginScreenPageFactoryObjects.userNameTextBox, "Testerauto1");
+						enterText(LoginScreenPageFactoryObjects.passWordTextBox,"Google@123" );     
+			buttonClick(LoginScreenPageFactoryObjects.loginButton);
+			pause(3000);
+			String actualTextValue	=driver.findElement(By.xpath(LoginScreenPageFactoryObjects.loginFailMSG)).getAttribute("innerHTML");
+		     System.out.println(actualTextValue);
+			Assert.assertTrue(actualTextValue.contains("Error: Incorrect login or password provided."));
+			
+//			String actualTextValue=	getAttribute(LoginScreenPageFactoryObjects.loginFailMSG);
+			
+			
+} catch (Exception e) {
+		
+		e.printStackTrace();
 	}
-
-	// Login unsuccesful with user name : Testerauto , Password : Google@12345
+	}
+	// Login unsuccessful with user name : Testerauto , Password : Google@12345
 	@Test(priority = 2)
-	public void verifyInvalidLogin_InvalidUserPassword() {
+	public void verifyInvalidLogin_InvalidUserPassword() throws Throwable {
 
-		driver.findElement(By.xpath("//a[normalize-space()='Login or register']")).click();
-		driver.findElement(By.xpath("//input[@id='loginFrm_loginname']")).sendKeys("Testerauto");
-
-		driver.findElement(By.xpath("//input[@id='loginFrm_password']")).sendKeys("Google@12345");
-
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+		try {
+			buttonClick(LoginScreenPageFactoryObjects.LoginOrRegisterButton);
+			enterText(LoginScreenPageFactoryObjects.userNameTextBox, "Testerauto");
+			
+			enterText(LoginScreenPageFactoryObjects.passWordTextBox,"Google@12345" );     
+			buttonClick(LoginScreenPageFactoryObjects.loginButton);
+			pause(3000);
+			String actualTextValue	=driver.findElement(By.xpath(LoginScreenPageFactoryObjects.loginFailMSG)).getAttribute("innerHTML");
+		     System.out.println(actualTextValue);
+			Assert.assertTrue(actualTextValue.contains("Error: Incorrect login or password provided."));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
 	}
 
 	// Login succesfully with user name : Testerauto , Password : Google@123
 	@Test(priority = 3)
-	public void abilityToLoginSuccessfully() {
-
-		driver.findElement(By.xpath("//a[normalize-space()='Login or register']")).click();
-		driver.findElement(By.xpath("//input[@id='loginFrm_loginname']")).sendKeys("Testerauto");
-
-		driver.findElement(By.xpath("//input[@id='loginFrm_password']")).sendKeys("Google@123");
-
-		driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+	public void abilityToLoginSuccessfully() throws Throwable {
+String ExpectedText = "Welcome back Ramya";
+		try {
+		buttonClick(LoginScreenPageFactoryObjects.LoginOrRegisterButton);
+			enterText(LoginScreenPageFactoryObjects.userNameTextBox, "Testerauto");
+			
+			enterText(LoginScreenPageFactoryObjects.passWordTextBox,"Google@123" );     
+			buttonClick(LoginScreenPageFactoryObjects.loginButton);
+			pause(3000);
+			captureScreen();
+			pause(3000);
+		String actualText=	getText(LocType.xpath,WelcomeScreenPageFactoryObjects.welcomeText);
+			
+			
+			Assert.assertTrue(actualText.contains(ExpectedText));
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+	@Test (priority = 4)
+	public void dummy() {
+		
+		System.out.println("none");
 	}
 
 }
