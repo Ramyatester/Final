85e478480e30e4ed0574548fef34b8f62f9498f2
Ramyatester <167738269+Ramyatester@users.noreply.github.com>
2024-05-14 20:41:20 +0530

    Delete eclipse-workspace/Flipkart directory

--- eclipse-workspace/Flipkart/src/test/java/base/AdvanageHomePageObjects.java
+++
@@ -1,7 +0,0 @@
-package base;
-
-public class AdvanageHomePageObjects {
-
-	public static final String menuSearch_xpath = "(//*[@id=\"menuSearch\"])[1]";
-	public static final String searchBar_xpath	="//*[@id='autoComplete']";
-}
