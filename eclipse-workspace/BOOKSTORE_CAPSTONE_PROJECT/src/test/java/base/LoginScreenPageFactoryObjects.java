edac39a782748433f7caf4e32872d648a17f5024
Ramyatester <167738269+Ramyatester@users.noreply.github.com>
2024-05-14 20:41:06 +0530

    Delete eclipse-workspace/BOOKSTORE_CAPSTONE_PROJECT directory

--- eclipse-workspace/BOOKSTORE_CAPSTONE_PROJECT/src/test/java/base/LoginScreenPageFactoryObjects.java
+++
@@ -1,9 +0,0 @@
-package base;
-
-public class LoginScreenPageFactoryObjects {
-
-	public static final String menuSearch_xpath = "(//*[@id=\"menuSearch\"])[1]";
-	public static final String searchBar_xpath = "//*[@id='autoComplete']";
-	//
-
-}
--- eclipse-workspace/BOOKSTORE_CAPSTONE_PROJECT/src/test/java/systemTests/WelcomeScreen.java
+++
@@ -1,7 +0,0 @@
-package systemTests;
-
-import base.BaseClass;
-
-public class WelcomeScreen extends BaseClass {
-
-}
